c9f96d84ff13a2e9e99b9a6858ec21b6710e950c
bayuadji <bayuadji@gmail.com>
2020-07-19 08:32:51 +0000

    add lambda and api

--- eksproject/eksproject_stack.py
+++ eksproject/eksproject_stack.py
@@ -1,4 +1,9 @@
-from aws_cdk import core
+from aws_cdk import(
+    core,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
+
 
 
 class EksprojectStack(core.Stack):
@@ -7,3 +12,14 @@ class EksprojectStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        my_lambda = _lambda.Function(
+            self, "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code   =_lambda.Code.asset('lambda'),
+            handler="hello.handler"
+        )
+
+        apigw.LambdaRestApi(
+            self, "Endpoint",
+            handler=my_lambda
+        )
